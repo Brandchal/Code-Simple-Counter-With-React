559da8eb6194412d5542cf57cf3c99a1343058df
Brandchal <Bchavarrial@hotmail.com>
2021-03-03 22:58:49 +0000

    update

--- src/js/component/SecondsCounter.js
+++ src/js/component/SecondsCounter.js
@@ -1,49 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../../styles/simpleCounter.css";
 
 export function SecondsCounter(props) {
-	let unit = props.seconds;
-	let ten = 0;
-	let hundred = 0;
-	let thousand = 0;
+	let one = props.seconds;
+	let two = 0;
+	let three = 0;
+	let four = 0;
 
-	if (unit > 9) {
-		ten = Math.trunc(props.seconds / 10);
-		unit = props.seconds % 10;
+	if (one > 9) {
+		two = Math.trunc(props.seconds / 10);
+		one = props.seconds % 10;
 
-		if (ten > 9) {
-			hundred = Math.trunc(ten / 10);
-			ten = ten % 10;
+		if (two > 9) {
+			three = Math.trunc(two / 10);
+			two = two % 10;
 
-			if (hundred > 9) {
-				thousand = Math.trunc(hundred / 10);
-				hundred = hundred % 10;
+			if (three > 9) {
+				four = Math.trunc(three / 10);
+				three = three % 10;
 
-				if (thousand > 9) {
-					thousand = "D";
-					hundred = "O";
-					ten = "E";
+				if (four > 9) {
+					four = "D";
+					three = "O";
+					two = "E";
 				}
 			}
 		}
 	}
 
 	return (
-		<div className="container-flex bg-dark text-white">
-			<div className="row">
-				<div className="col-3"></div>
-				<div className="col-6 d-flex justify-content-center">
-					<div className="d-inline-block fs-1">
-						<i className="fas fa-stopwatch"></i>
-					</div>
-
-					<div className="d-inline-block fs-1">{thousand}</div>
-					<div className="d-inline-block fs-1">{hundred}</div>
-					<div className="d-inline-block fs-1">{ten}</div>
-					<div className="d-inline-block fs-1">{unit}</div>
-				</div>
-				<div className="col-3"></div>
+		<div className="counter">
+			<div className="d-inline-block fs-1">
+				<i className="fas fa-stopwatch"></i>
 			</div>
+
+			<div className="d-inline-block fs-1">{four}</div>
+			<div className="d-inline-block fs-1">{three}</div>
+			<div className="d-inline-block fs-1">{two}</div>
+			<div className="d-inline-block fs-1">{one}</div>
 		</div>
 	);
 }
